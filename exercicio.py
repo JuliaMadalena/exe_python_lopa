f25715012d546eb73cf6f24e4d5275e8cbc3e4a1
JuliaMadalena <madalena27ju@gmail.com>
2025-02-12 22:41:36 -0300

    exercicio aula 02

---
+++ exercicio.py
@@ -0,0 +1,14 @@
+# exercício da aula 2, 12/02
+
+maca = 10
+laranja = 400
+maracuja = 27
+goiaba = maca + 10
+
+soma = maca + laranja + maracuja
+print(soma)
+
+print(goiaba)
+
+media = (maca + laranja + maracuja + goiaba)/4
+print(media)
